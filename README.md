dcece1f2d30259493f0f534bcab5ac256cbff3a8
Brian M. Viana <brianmviana@gmail.com>
2019-08-07 23:08:16 -0300

    backend finalizado - Dia 02 semana Omnistack

--- README.md
+++ README.md
@@ -11,8 +11,6 @@ cd backend
 # instala as dependencias
 npm install
 
-# Adicione seu projeto do Firebase na main.js
-
 # Inicializa a aplicação vue na url: localhost:8080
 npm start
 ```
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
 const server = express();
 
-server.get('/',(req, res) => {
-    
-});
+mongoose.connect('mongodb+srv://brianmviana:brianmviana@cluster0-9nknc.mongodb.net/tindev?retryWrites=true&w=majority', {useNewUrlParser : true});
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
 
 server.listen(3000);
